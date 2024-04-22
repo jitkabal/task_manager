9c846860d8adccd1baafd9874eced86121779b5b
jitkabal <jitkabaldwin@gmail.com>
2024-04-22 20:09:01 +0200

    ukol 5 a 6

--- task_manager.py
+++ task_manager.py
@@ -12,6 +12,16 @@ databaze_tasku = {}
 databaze_tasku[aktualni_klic] = Task(...)
 aktualni_klic += 1
 
+def add_task(description:str)-> None:
+    task = Task(description)
+    databaze_tasku[aktualni_klic] = task
+    aktualni_klic += 1
+
+def remove_task(task_id:int) -> None:
+    if task_id in databaze_tasku:
+        databaze_tasku.pop(task_id)    
+
+def 
     def __str__(self):
         return f"{self.timestamp} + {self.description}"    
     
